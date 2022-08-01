4124ec704e12a29038cd6906b5376db1c8713393
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-01 18:47:00 +0300

    game bot - work with Object Model Document

--- js/script.js
+++ js/script.js
@@ -8,46 +8,47 @@ const guessGame = () => {
   let numberPuzzle = Math.floor(Math.random() * 10 + 1);
   let attempts = 5;
   let count = 0;
+  const numberUser = document.querySelector(".number");
+  const btn = document.querySelector(".btn_guess");
+  const repeatBtn = document.querySelector(".btn_repeat");
+  let message = document.querySelector(".message");
+  const close = document.querySelector(".btn_close");
+
+  close.addEventListener("click", () => {
+    message.innerText = "Игра окончена. Можете попробовать снова";
+    guessGame();
+  });
 
   const checkUserNumber = () => {
-    let userAnswer = prompt("Угадай число от 1 до 10");
+    let userAnswer = numberUser.value;
     let userAnswerNum = +userAnswer;
-
-    const repeatGame = (answer) => {
-      if (answer) {
-        guessGame();
-      } else {
-        alert("Игра окончена");
-        return;
-      }
+    const getBtnRepeat = () => {
+      repeatBtn.classList = "btn_repeat_visibile";
+      repeatBtn.addEventListener("click", guessGame);
+      repeatBtn.addEventListener("click", () => (numberUser.value = ""));
     };
     count++;
-    if (userAnswer === null) {
+
+    if (userAnswerNum > 10 || userAnswer === "" || userAnswerNum <= 0 || isNaN(userAnswerNum)) {
       count--;
-      alert("Игра окончена");
-    } else if (userAnswerNum > 10 || userAnswer === "" || userAnswerNum <= 0 || isNaN(userAnswerNum)) {
-      count--;
-      alert("Введите число от 1 до 10");
-      checkUserNumber();
+      message.innerText = "Введите число от 1 до 10";
+      numberUser.addEventListener("click", () => (numberUser.value = ""));
     } else if (count < 5 && userAnswerNum > numberPuzzle) {
-      alert(`Загаданное число МЕНЬШЕ. Осталось попыток ${attempts - count}`);
-      checkUserNumber();
+      message.innerText = `Загаданное число МЕНЬШЕ. Осталось попыток ${attempts - count}`;
+      numberUser.addEventListener("click", () => (numberUser.value = ""));
     } else if (count < 5 && userAnswerNum < numberPuzzle) {
-      alert(`Загаданное число БОЛЬШЕ. Осталось попыток ${attempts - count}`);
-      checkUserNumber();
+      message.innerText = `Загаданное число БОЛЬШЕ. Осталось попыток ${attempts - count}`;
+      numberUser.addEventListener("click", () => (numberUser.value = ""));
     } else if (count <= 5 && userAnswerNum === numberPuzzle) {
-      let answer = confirm(
-        `Поздравляю, Вы угадали c ${count} попытки загаданное число: ${numberPuzzle} ! Хотели бы сыграть еще?`
-      );
-      repeatGame(answer);
+      message.innerText = `Поздравляю, Вы угадали c ${count} попытки загаданное число: ${numberPuzzle} ! Хотели бы сыграть еще? Нажмите кнопку повтора`;
+      getBtnRepeat();
     } else {
-      let answer = confirm("Попытки закончились, хотите сыграть еще?");
-      repeatGame(answer);
+      message.innerText = "Попытки закончились, хотите сыграть еще? Нажмите кнопку повтора";
+      getBtnRepeat();
     }
     return;
   };
-
-  checkUserNumber();
+  btn.addEventListener("click", checkUserNumber);
   return;
 };
 guessGame();
