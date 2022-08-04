e6aeea137177bbf2f027933bace4ce73ad043423
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-04 10:27:36 +0300

    homework-06

--- js/script.js
+++ js/script.js
@@ -3,81 +3,76 @@
 const app = () => {
   let money = 70000;
   let profit = "Фриланс, репетиторство";
-  let expenses = "Питание, Проезд, Развлечения, Медицина";
   let purpose = 100000;
-  let period = 6;
   let budgetDay = Math.floor(money / 30);
 
-  money = +prompt("Ваш месячный доход (руб)?", 100000);
-  console.log(`Месячный доход равен ${money} рублей`);
+  const getMessageError = (message) => {
+    let money = +prompt(message);
+    while (isNaN(money)) {
+      alert("Введите число!");
+      money = +prompt(message);
+    }
+    return money;
+  };
 
+  money = getMessageError("Ваш месячный доход (руб)?");
   purpose = +prompt("Сколько хотите накопить (руб)?", 150000);
-  console.log(`Цель накопить ${purpose} рублей`);
 
-  expenses = prompt(
-    "Перечислите возможные расходы за рассчитываемый период через запятую. Пример: питание, проезд",
-    "питание, проезд, развлечения, медицина"
+  let amount = getMessageError("Во сколько обойдуться обязательные статьи расходов (руб)?");
+  let extraMoney = getMessageError(
+    `Перечислите возможный доход за ваши дополнительные работы (руб): ${profit.toLowerCase()}?`
   );
-  let newArrExpenses = expenses.toLowerCase().split(", ");
-  console.log("Обязательные статьи расходов:");
-  console.log(newArrExpenses);
 
-  let amount = +prompt(
-    "Во сколько обойдуться обязательные статьи расходов (руб)?",
-    60000
-  );
-  console.log(`Ваши обязательные расходы: ${amount} рублей`);
-
-  let deposit = confirm("Есть ли у вас вклад в банке?");
-  if (deposit) {
-    console.log("Вклад в банке есть");
-  } else {
-    console.log("Вклада в банке нет");
-  }
-
-  let extraMoney = +prompt(
-    `Перечислите возможный доход за ваши дополнительные работы (руб): ${profit.toLowerCase()}?`,
-    30000
-  );
-  console.log(`Возможный доход за доп.работы ${extraMoney} рублей`);
-
-  /*lesson-04*/
-
-  /* Объявить функцию getAccumulatedIncome. Возврат накоплений за месяц (Доходы минус расходы)*/
   const getAccumulatedIncome = (money, extraMoney, amount) => money + extraMoney - amount;
-
-  /* Объявить переменную accumulatedIncome и присвоить результат от getAccumulatedIncome*/
   let accumulatedIncome = getAccumulatedIncome(money, extraMoney, amount);
 
-  /*Объявить функцию getTargetMonth. Подсчитать, за какой период будет достигнута цель, зная результат месячного накопления (getAccumulatedIncome)*/
-  const getTargetMonth = (purpose, income) => Math.ceil(purpose / income);
+  const getTargetMonth = (purpose, income) => {
+    let targetMonth = Math.ceil(purpose / income);
+    if (targetMonth > 0 && targetMonth != Infinity) console.log("Цель будет достигнута");
+    if (targetMonth <= 0 || targetMonth === Infinity) console.log("Цель не будет достигнута");
+    return targetMonth;
+  };
 
-  /*budgetDay рассчитываем исходя из значения месячного накопления (accumulatedIncome)*/
   budgetDay = Math.floor(accumulatedIncome / 30);
-
-  if (budgetDay > 6000) {
-    console.log("У вас высокий уровень дохода");
-  }
-  if (budgetDay >= 3000 && budgetDay <= 6000) {
-    console.log("У вас средний уровень дохода");
-  }
-  if (budgetDay >= 0 && budgetDay < 3000) {
-    console.log("К сожалению, у вас уровень дохода ниже среднего");
-  }
-  if (budgetDay < 0) {
-    console.log("Что-то пошло не так");
-  }
-
-  /*Очистить консоль логи и должны остаться:*/
-  console.clear();
-
   console.log(
     `Ваш бюджет на месяц с учетом Ваших расходов составляет: ${getAccumulatedIncome(money, extraMoney, amount)} рублей`
   );
   console.log(
-    `Ваща цель накопить ${purpose} с учетом Ваших расходов будет достигнута через ${getTargetMonth(purpose, accumulatedIncome)} месяца(-ев)`
+    `Ваща цель накопить ${purpose} рублей с учетом Ваших расходов будет достигнута через ${getTargetMonth(
+      purpose,
+      accumulatedIncome
+    )} месяца(-ев)`
   );
   console.log(`Дневной бюджет ${budgetDay} рублей`);
 };
 
 app();
+console.log("_____________________");
+
+/*усложненное задание*/
+
+const getNumbers = () => {
+  const numbers = [];
+  numbers.push("28", "58", "35", "48", "35", "42", "78");
+  console.log(`Исходный массив: ${numbers}`);
+
+  let numbersFiltered = numbers.filter((item) => item[0] === "4" || item[0] === "2");
+  console.log(`Элементы массива начинаются с цифр 2 и 4: ${numbersFiltered}`);
+};
+
+getNumbers();
+
+const getUserString = (str) => {
+  let userString = str.trim();
+  if (typeof str === "string" && userString.length < 30) {
+    console.log(`Вы ввели строку: ${userString}`);
+  }
+  if (typeof str === "string" && userString.length >= 30) {
+    console.log(`Вы ввели строку: ${userString.slice(0, 30)}...`);
+    console.log(`Длина видимой части строки ${userString.slice(0, 30).length}`);
+  }
+};
+getUserString("  яблоко    ");
+getUserString(
+  "     Наше дело не так однозначно, как может показаться: сплочённость команды профессионалов однозначно определяет каждого участника как способного принимать собственные решения касаемо переосмысления внешнеэкономических политик.     "
+);
