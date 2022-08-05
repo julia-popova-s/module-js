4798b109cc24631c4c7766749be3ca06e6fe0b12
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-05 16:11:18 +0300

    added function getData

--- js/script.js
+++ js/script.js
@@ -1,45 +1,54 @@
 //практика использования запроса на сервер, обработка запросов, и обработка данных по заданиям
-fetch("https://reqres.in/api/users?per_page=12")
-  .then((res) => {
-    return res.json();
-  })
+"use strict";
+const app = () => {
+  const getData = (url) =>
+    new Promise((resolve, reject) =>
+      fetch(url)
+        .then((response) => response.json())
+        .then((json) => resolve(json))
+        .catch((error) => reject(error))
+    );
 
-  .then((body) => {
-    // Получить данные 12 пользователей
-    console.log("Пункт №1:");
-    console.log(`Данные ${body.data.length} пользователей:`);
-    console.log(body.data);
+  getData("https://reqres.in/api/users?per_page=12")
+    .then((body) => {
+      // Получить данные 12 пользователей
+      console.log("Пункт №1:");
+      console.log(`Данные ${body.data.length} пользователей:`);
+      console.log(body.data);
 
-    // Вывести в консоль фамилии всех пользователей в цикле
-    console.log("_______________________________");
-    console.log("Пункт №2:");
-    console.log("Фамилии пользователей:");
-    body.data.forEach((item) => {
-      console.log(item.last_name);
-    });
+      // Вывести в консоль фамилии всех пользователей в цикле
+      console.log("_______________________________");
+      console.log("Пункт №2:");
+      console.log("Фамилии пользователей:");
+      body.data.forEach((item) => {
+        console.log(item.last_name);
+      });
 
-    // Вывести все данные всех пользователей, фамилия которых начинается на F
-    console.log("_______________________________");
-    console.log("Пункт №3:");
-    console.log("Данные пользователей, фамилия которых начинается c 'F':");
-    body.data.forEach((item) => {
-      if (item.last_name[0] === "F") console.log(item.last_name);
-    });
+      // Вывести все данные всех пользователей, фамилия которых начинается на F
+      console.log("_______________________________");
+      console.log("Пункт №3:");
+      console.log("Данные пользователей, фамилия которых начинается c 'F':");
+      body.data.forEach((item) => {
+        if (item.last_name[0] === "F") console.log(item.last_name);
+      });
 
-    /* Вывести следующее предложение: Наша база содержит данные следующих пользователей: 
+      /* Вывести следующее предложение: Наша база содержит данные следующих пользователей: 
     и далее в этой же строке через запятую имена и фамилии всех пользователей. Использовать метод reduce*/
-    console.log("_______________________________");
-    console.log("Пункт №4:");
-    let result =
-      "Наша база содержит данные следующих пользователей: " +
-      body.data.reduce((sum, item) => `${item.last_name} ${item.first_name}, ` + sum, "");
-    console.log(result.slice(0, result.length - 2) + ".");
+      console.log("_______________________________");
+      console.log("Пункт №4:");
+      let result =
+        "Наша база содержит данные следующих пользователей: " +
+        body.data.reduce((sum, item) => `${item.last_name} ${item.first_name}, ` + sum, "");
+      console.log(result.slice(0, result.length - 2) + ".");
 
-    // Вывести названия всех ключей в объекте пользователя.
-    console.log("_______________________________");
-    console.log("Пункт №5:");
-    console.log("Ключи пользователя: " + Object.keys(body.data[0]).join(", ") + ".");
-    /*body.data.forEach((item) => {
+      // Вывести названия всех ключей в объекте пользователя.
+      console.log("_______________________________");
+      console.log("Пункт №5:");
+      console.log("Ключи пользователя: " + Object.keys(body.data[0]).join(", ") + ".");
+      /*body.data.forEach((item) => {
       console.log(`${item.last_name} ${item.first_name}: ` + Object.keys(item));
     });*/
-  });
+    })
+    .catch((error) => console.log(error.message));
+};
+app();
