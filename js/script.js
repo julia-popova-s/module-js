51bd3496854e2b4e5595b64f2332afeed1d7f046
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-05 11:08:45 +0300

    homework-07

--- js/script.js
+++ js/script.js
@@ -1,78 +1,45 @@
-"use strict";
+//практика использования запроса на сервер, обработка запросов, и обработка данных по заданиям
+fetch("https://reqres.in/api/users?per_page=12")
+  .then((res) => {
+    return res.json();
+  })
 
-const app = () => {
-  let money = 70000;
-  let profit = "Фриланс, репетиторство";
-  let purpose = 100000;
-  let budgetDay = Math.floor(money / 30);
+  .then((body) => {
+    // Получить данные 12 пользователей
+    console.log("Пункт №1:");
+    console.log(`Данные ${body.data.length} пользователей:`);
+    console.log(body.data);
 
-  const getMessageError = (message) => {
-    let money = +prompt(message);
-    while (isNaN(money)) {
-      alert("Введите число!");
-      money = +prompt(message);
-    }
-    return money;
-  };
+    // Вывести в консоль фамилии всех пользователей в цикле
+    console.log("_______________________________");
+    console.log("Пункт №2:");
+    console.log("Фамилии пользователей:");
+    body.data.forEach((item) => {
+      console.log(item.last_name);
+    });
 
-  money = getMessageError("Ваш месячный доход (руб)?");
-  purpose = +prompt("Сколько хотите накопить (руб)?", 150000);
+    // Вывести все данные всех пользователей, фамилия которых начинается на F
+    console.log("_______________________________");
+    console.log("Пункт №3:");
+    console.log("Данные пользователей, фамилия которых начинается c 'F':");
+    body.data.forEach((item) => {
+      if (item.last_name[0] === "F") console.log(item.last_name);
+    });
 
-  let amount = getMessageError("Во сколько обойдуться обязательные статьи расходов (руб)?");
-  let extraMoney = getMessageError(
-    `Перечислите возможный доход за ваши дополнительные работы (руб): ${profit.toLowerCase()}?`
-  );
+    /* Вывести следующее предложение: Наша база содержит данные следующих пользователей: 
+    и далее в этой же строке через запятую имена и фамилии всех пользователей. Использовать метод reduce*/
+    console.log("_______________________________");
+    console.log("Пункт №4:");
+    let result =
+      "Наша база содержит данные следующих пользователей: " +
+      body.data.reduce((sum, item) => `${item.last_name} ${item.first_name}, ` + sum, "");
+    console.log(result.slice(0, result.length - 2) + ".");
 
-  const getAccumulatedIncome = (money, extraMoney, amount) => money + extraMoney - amount;
-  let accumulatedIncome = getAccumulatedIncome(money, extraMoney, amount);
-
-  const getTargetMonth = (purpose, income) => {
-    let targetMonth = Math.ceil(purpose / income);
-    if (targetMonth > 0 && targetMonth != Infinity) console.log("Цель будет достигнута");
-    if (targetMonth <= 0 || targetMonth === Infinity) console.log("Цель не будет достигнута");
-    return targetMonth;
-  };
-
-  budgetDay = Math.floor(accumulatedIncome / 30);
-  console.log(
-    `Ваш бюджет на месяц с учетом Ваших расходов составляет: ${getAccumulatedIncome(money, extraMoney, amount)} рублей`
-  );
-  console.log(
-    `Ваща цель накопить ${purpose} рублей с учетом Ваших расходов будет достигнута через ${getTargetMonth(
-      purpose,
-      accumulatedIncome
-    )} месяца(-ев)`
-  );
-  console.log(`Дневной бюджет ${budgetDay} рублей`);
-};
-
-app();
-console.log("_____________________");
-
-/*усложненное задание*/
-
-const getNumbers = () => {
-  const numbers = [];
-  numbers.push("28", "58", "35", "48", "35", "42", "78");
-  console.log(`Исходный массив: ${numbers}`);
-
-  let numbersFiltered = numbers.filter((item) => item[0] === "4" || item[0] === "2");
-  console.log(`Элементы массива начинаются с цифр 2 и 4: ${numbersFiltered}`);
-};
-
-getNumbers();
-
-const getUserString = (str) => {
-  let userString = str.trim();
-  if (typeof str === "string" && userString.length < 30) {
-    console.log(`Вы ввели строку: ${userString}`);
-  }
-  if (typeof str === "string" && userString.length >= 30) {
-    console.log(`Вы ввели строку: ${userString.slice(0, 30)}...`);
-    console.log(`Длина видимой части строки ${userString.slice(0, 30).length}`);
-  }
-};
-getUserString("  яблоко    ");
-getUserString(
-  "     Наше дело не так однозначно, как может показаться: сплочённость команды профессионалов однозначно определяет каждого участника как способного принимать собственные решения касаемо переосмысления внешнеэкономических политик.     "
-);
+    // Вывести названия всех ключей в объекте пользователя.
+    console.log("_______________________________");
+    console.log("Пункт №5:");
+    console.log("Ключи пользователя: " + Object.keys(body.data[0]).join(", ") + ".");
+    /*body.data.forEach((item) => {
+      console.log(`${item.last_name} ${item.first_name}: ` + Object.keys(item));
+    });*/
+  });
