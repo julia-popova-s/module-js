e425bbbf0f8f3c536987639b565d4071c518283c
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-10 13:01:09 +0300

    interim result 2

--- js/script.js
+++ js/script.js
@@ -9,52 +9,80 @@ const app = () => {
   const inputs = formReg.querySelectorAll(".registration-form__input");
   const labels = formReg.querySelectorAll(".registration-form__label");
   const buttonReg = formReg.querySelector(".registration-form__btn");
+  const alerts = formReg.querySelectorAll(".registration-form__alert");
+
   const userEmail = inputs[0];
   const userParol = inputs[1];
+  const labelEmail = labels[0];
+  const labelParol = labels[1];
+
   const checkbox = formReg.querySelector(".checkbox");
   const checkboxMark = formReg.querySelector(".checkbox__mark");
-  const checkboxLabel = formReg.querySelector(".checkbox__label");
-  let message = "<p class='registration-form__message'>Поле обязательно для заполнения</p>";
+  let message = "Поле обязательно для заполнения";
 
   const checkForEmptyLine = (input, class_input, label, class_label, message) => {
-    input.insertAdjacentHTML("afterend", message);
+    alerts.forEach((item) => (item.innerText = message));
     input.classList.add(class_input);
     label.classList.add(class_label);
   };
+
   const removeClass = (input, class_input, label, class_label) => {
     input.classList.remove(class_input);
     label.classList.remove(class_label);
   };
 
+  const removeAlert = (element, event, label) => {
+    element.addEventListener("input", () => {
+      removeClass(element, "border_red", label, "invalid");
+    });
+  };
+
   buttonReg.addEventListener("click", (e) => {
     e.preventDefault();
-
+    let counter = 0;
     if (userEmail.value === "") {
-      checkForEmptyLine(userEmail, "border_red", labels[0], "invalid", message);
+      checkForEmptyLine(userEmail, "border_red", labelEmail, "invalid", message);
+      alerts[0].innerText = "";
+    }
+    if (!validateEmail(userEmail.value) && userEmail.value != "") {
+      alerts[0].innerText = "Email невалидный";
     }
     if (userParol.value === "") {
-      checkForEmptyLine(userParol, "border_red", labels[1], "invalid", message);
+      checkForEmptyLine(userParol, "border_red", labelParol, "invalid", message);
+      alerts[1].innerText = "";
     }
-    if (!checkboxMark.cheked) {
+    if (userParol.value.length < 8 && userParol.value != "") {
+      alerts[1].innerText = "Пароль должен содержать как минимум 8 символов";
+    }
+    if (!checkboxMark.checked) {
       checkForEmptyLine(checkbox, "invalid", checkboxMark, "border_red", message);
     }
+    if (checkboxMark.checked) {
+      alerts[2].innerText = "";
+    }
+    if (userEmail.value != "") removeAlert(userEmail, "input", labelEmail);
+    if (userParol.value != "") removeAlert(userParol, "input", labelEmail);
 
-    let alerts = document.querySelectorAll(".registration-form__message");
+    if (validateEmail(userEmail.value) && userParol.value.length >= 8 && checkboxMark.checked) {
+      alerts.forEach((item) => (item.innerText = ""));
 
-    userEmail.addEventListener("input", () => {
-      removeClass(userEmail, "border_red", labels[0], "invalid");
-      alerts[0].remove();
-    });
+      let userData = {};
+      userData.email = userEmail.value;
+      userData.password = userParol.value;
 
-    userParol.addEventListener("input", () => {
-      removeClass(userParol, "border_red", labels[1], "invalid");
-      alerts[1].remove();
-    });
+      console.log(userData);
+      // formReg.submit();
+    }
     checkboxMark.addEventListener("change", () => {
       removeClass(checkbox, "invalid", checkboxMark, "border_red");
+      alerts[2].innerText = "";
     });
 
     return;
   });
 };
 app();
+/*{
+    email: johndoe@mail.ru
+    password: password 
+*/
