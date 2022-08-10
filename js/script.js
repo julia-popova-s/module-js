0e788601e5919a44c46fb9c801c574ccd82a4230
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-10 11:37:11 +0300

    added color changes to form

--- js/script.js
+++ js/script.js
@@ -14,31 +14,46 @@ const app = () => {
   const checkbox = formReg.querySelector(".checkbox");
   const checkboxMark = formReg.querySelector(".checkbox__mark");
   const checkboxLabel = formReg.querySelector(".checkbox__label");
-  console.log(checkboxLabel.style);
+  let message = "<p class='registration-form__message'>Поле обязательно для заполнения</p>";
+
+  const checkForEmptyLine = (input, class_input, label, class_label, message) => {
+    input.insertAdjacentHTML("afterend", message);
+    input.classList.add(class_input);
+    label.classList.add(class_label);
+  };
+  const removeClass = (input, class_input, label, class_label) => {
+    input.classList.remove(class_input);
+    label.classList.remove(class_label);
+  };
+
   buttonReg.addEventListener("click", (e) => {
     e.preventDefault();
-    let message;
-    const checkEmpty = (element, message) => {
-      // message = "<p class='registration-form__message'>Поле обязательно для заполнения</p>";
-      element.insertAdjacentHTML("afterend", message);
-      element.classList.add("registration-form__input_color_red");
-    };
+
     if (userEmail.value === "") {
-      message = "<p class='registration-form__message'>Поле обязательно для заполнения</p>";
-      userEmail.insertAdjacentHTML("afterend", message);
-      userEmail.classList.add("registration-form__input_border_red");
-      labels[0].classList.add("invalid");
+      checkForEmptyLine(userEmail, "border_red", labels[0], "invalid", message);
     }
     if (userParol.value === "") {
-      message = "<p class='registration-form__message'>Поле обязательно для заполнения</p>";
-      userParol.insertAdjacentHTML("afterend", message);
-      userParol.classList.add("registration-form__input_border_red");
-      labels[1].classList.add("invalid");
+      checkForEmptyLine(userParol, "border_red", labels[1], "invalid", message);
     }
     if (!checkboxMark.cheked) {
-      message = "<p class='registration-form__message'>Поле обязательно для заполнения</p>";
-      checkbox.insertAdjacentHTML("afterend", message);
+      checkForEmptyLine(checkbox, "invalid", checkboxMark, "border_red", message);
     }
+
+    let alerts = document.querySelectorAll(".registration-form__message");
+
+    userEmail.addEventListener("input", () => {
+      removeClass(userEmail, "border_red", labels[0], "invalid");
+      alerts[0].remove();
+    });
+
+    userParol.addEventListener("input", () => {
+      removeClass(userParol, "border_red", labels[1], "invalid");
+      alerts[1].remove();
+    });
+    checkboxMark.addEventListener("change", () => {
+      removeClass(checkbox, "invalid", checkboxMark, "border_red");
+    });
+
     return;
   });
 };
