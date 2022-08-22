b003c78c20adfff2a80e00fc456d71e5226fd941
Julia Popova <yuliapopova.s@yandex.ru>
2022-08-22 16:36:19 +0300

    my nonideal solution

--- js/script.js
+++ js/script.js
@@ -23,16 +23,6 @@ const getSortedList = (list, classItem, message) => {
   }
 };
 getSortedList(booksList, ".book", "Книга");
-// for (let k = 0; k < booksList.length - 1; k++) {
-//   for (let i = 0; i < booksList.length - 1; i++) {
-//     booksList = Array.from(document.querySelectorAll(".book"));
-//     let first = +booksList[i].innerText[6];
-//     let second = +booksList[i + 1].innerText[6];
-//     if (first > second) {
-//       booksList[i].before(booksList[i + 1]);
-//     }
-//   }
-// }
 
 // - Заменить картинку заднего фона на другую из папки image
 body.style.cssText = "background-image:url('./image/you-dont-know-js.jpg')";
@@ -42,14 +32,50 @@ const titles = document.querySelectorAll(".book h2 a");
 titles[2].textContent = "Книга 3. this и Прототипы Объектов";
 
 // - Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
-let chapterList = Array.from(document.querySelectorAll(".book__chapter-list li"));
+
+//получили список книг
+const chapterList = Array.from(document.querySelectorAll(".book__chapter-list"));
 console.log(chapterList);
+//получили список глав книг 2 и 5
+const chapterListBook2 = Array.from(chapterList[1].children);
+const chapterListBook5 = Array.from(chapterList[4].children);
 
-let listChap = chapterList.filter((item) => item.innerText.indexOf("Глава") != -1);
-getSortedList(listChap, ".book__chapter-list li", "Глава");
+//функция сортировки глав
+const mySort = (list) => {
+  list.sort((a, b) => {
+    if (a.innerText < b.innerText) {
+      return -1;
+    }
+    if (a.innerText > b.innerText) {
+      return 1;
+    }
+    return 0;
+  });
+};
 
-let listChapApp = chapterList.filter((item) => item.innerText.indexOf("Приложение") != -1);
-console.log(listChapApp);
-// getSortedList(listChapApp, ".book__chapter-list li", "Приложение");
+const getChapterList = (chapterList) => {
+  const list = [];
+  const listOther = chapterList.filter(
+    (item) => item.innerText.includes("Глава") === false && item.innerText.includes("Приложение") === false
+  );
+  const listChap = chapterList.filter((item) => item.innerText.indexOf("Глава") != -1);
+  mySort(listChap);
+  const listChapApp = chapterList.filter((item) => item.innerText.indexOf("Приложение") != -1);
+  mySort(listChapApp);
+  listOther.forEach((item) => list.push(item.innerText));
+  listChap.forEach((item) => list.push(item.innerText));
+  listChapApp.forEach((item) => list.push(item.innerText));
+  return list;
+};
+const resultList2 = getChapterList(chapterListBook2);
+chapterListBook2.forEach((item, i) => (item.innerText = resultList2[i]));
+
+const resultList5 = getChapterList(chapterListBook5);
+chapterListBook5.forEach((item, i) => (item.innerText = resultList5[i]));
 
 // - в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
+const book6 = Array.from(chapterList[5].children);
+console.log(book6);
+const item = document.createElement("li");
+item.innerText = "Глава 8: За пределами ES6";
+book6[8].append(item);
